7b8017976d6250a6cf2c1fc01bed29b2f95f84ae
xlzy <1017866168@qq.com>
2021-06-08 12:15:53 +0800

    feat：完成基础搭建

--- src/components/Feature.vue
+++ src/components/Feature.vue
@@ -23,14 +23,14 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: "Feature",
+  name: 'Feature',
   props: {
     title: { type: String, required: true },
     text: { type: String, required: true },
     url: { type: String, required: true },
   },
-});
+})
 </script>
